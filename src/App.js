3ff775337a8d8ee0d423cc153548ebfad4a0a5c1
Jiyaho <masqu2n@gmail.com>
2023-02-16 00:08:18 +0900

    2023-02-16

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./routes/Main";
-import Forecast from "./routes/Forecast";
-import Outfit from "./routes/Outfit";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/react-how_is_the_weather" element={<Main />} />
-        <Route
-          path="/react-how_is_the_weather/forecast"
-          element={<Forecast />}
-        />
-        <Route path="/react-how_is_the_weather/outfit" element={<Outfit />} />
       </Routes>
     </Router>
   );

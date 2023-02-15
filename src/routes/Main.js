a5766b165fe317f3836f6937952e327d7807392c
Jiyaho <masqu2n@gmail.com>
2023-02-16 00:10:00 +0900

    2023-02-16

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -2,14 +2,12 @@ import WeatherApi from "../components/WeatherApi";
 import styles from "../css/App.module.css";
 import Background from "../components/Background";
 import Header from "../components/Header";
-import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 
 function Main() {
   return (
     <div>
       <div className={styles.container}>
-        {/* <Nav /> */}
         <Header />
         <WeatherApi />
       </div>
